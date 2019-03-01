22bd8b026527bf3600551fc5dc7100aed295ee6d
Jerome R <jerome.olivier@gmail.com>
2019-03-01 17:01:42 +0100

    new createfile debug

--- go/src/cfile.go
+++ go/src/cfile.go
@@ -2,6 +2,7 @@ package main
 
 import(
 	"fmt"
+	"os"
 	"io/ioutil"
 )
 
@@ -12,12 +13,25 @@ func check(e error){
 	}
 }
 
-
 func main(){
 
+	fmt.Println("Creation du fichier /tmp/data1.txt --- 1ere façon: ioutil")
+
 	s1 := []byte("Hello World! \n Create file ;-)\n")
-	f := ioutil.WriteFile("/tmp/data1.txt", s1, 0644)
-	check(f)
+	f4 := ioutil.WriteFile("/tmp/data1.txt", s1, 0644)
+	check(f4)
 
 
+	fmt.Println("Lecture du fichier /tmp/data1.txt ")
+	f5, err := os.Open("/tmp/data1.txt")
+	check(err)
+
+	buf2 := make([]byte, 100)
+	for {
+		cpt, err := f5.Read(buf2)
+		check(err)
+		if cpt > 0 {
+			fmt.Print(string(buf2[:cpt]))
+		}
+	}
 }
